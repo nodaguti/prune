cecb6c8a683c815b9c3c1d16c964bc02ffc43eae
nodaguti <nodaguti@gmail.com>
2018-08-20 11:47:49 +0900

    Add a feature to make branch filters configurable

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,45 @@ import { post } from './slack';
 import { daysFromToday } from './utils';
 
 declare var global: any;
-declare const process: any;
+declare const process: {
+  env: { [key: string]: string };
+};
 
 global.main = (): void => {
-  const owner: string = process.env.GITHUB_REPO_OWNER;
-  const repo: string = process.env.GITHUB_REPO_NAME;
+  const {
+    GITHUB_REPO_OWNER: owner,
+    GITHUB_REPO_NAME: repo,
+    GITHUB_BRANCH_NAME_FILTER: nameFilter,
+    GITHUB_BRANCH_AGE_THRESHOLD: ageThreshold,
+    GITHUB_BRANCH_AGE_THRESHOLD_SHOWN_AS_OLD: ageThresholdShownAsOld,
+  } = process.env;
+
+  const nameFilterReg = new RegExp(nameFilter);
 
   const branches = fetchBranches(owner, repo);
-  const featureBranches = branches.filter((branch) => /^feature[\-\/]/.test(branch.name)).map((branch) => {
+  const featureBranches = branches.filter((branch) => nameFilterReg.test(branch.name)).map((branch) => {
     const { name } = branch;
     return fetchBranch(owner, repo, name);
   });
-  const attachments = featureBranches.map((branch) => {
-    const days = daysFromToday(new Date(branch.commit.commit.author.date));
-    const stale = days >= 30;
+  const attachments = featureBranches
+    .map((branch) => {
+      const days = daysFromToday(new Date(branch.commit.commit.author.date));
 
-    return {
-      fallback: `${branch.name}: ${branch.commit.commit.author.date}`,
-      title: branch.name,
-      title_link: branch._links.html,
-      text: `Last commit created by ${branch.commit.commit.author.name}, ${days} days ago`,
-      color: stale ? 'danger' : undefined,
-    };
-  });
+      if (days >= Number(ageThreshold)) {
+        return null;
+      }
+
+      const stale = days >= Number(ageThresholdShownAsOld);
+
+      return {
+        fallback: `${branch.name}: ${branch.commit.commit.author.date}`,
+        title: branch.name,
+        title_link: branch._links.html,
+        text: `Last commit created by ${branch.commit.commit.author.name}, ${days} days ago`,
+        color: stale ? 'danger' : undefined,
+      };
+    })
+    .filter((attachment) => !!attachment);
 
   post(attachments);
 };
